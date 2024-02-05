b889753ce612a9d38529d1519d82939ff4a1ef27
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2024-02-05 17:10:44 +0900

    feat: add docs and logging with fluent bit

--- README.md
+++ README.md
@@ -1,58 +1,15 @@
+# ECS on Fargate with IPv6 VPC
 
-# Welcome to your CDK Python project!
+IPv6を有効にしたVPC上でECSサービスを動作させるサンプルです。
+ECSサービスで動作するアプリケーションは [amazon-ecs-sample](https://hub.docker.com/r/amazon/amazon-ecs-sample) です。
 
-This is a blank project for CDK development with Python.
+## 開発方法
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+CDK Pythonプロジェクトです。以下の手順にしたがって開発してください。
 
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the `.venv`
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
+1. `source .venv/bin/activate` でvenvを有効化する
+2. `cdk diff` で作成されるリソースを確認する
+3. `cdk deploy` でリソースを作成する
 
-To manually create a virtualenv on MacOS and Linux:
-
-```
-$ python3 -m venv .venv
-```
-
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
-
-```
-$ source .venv/bin/activate
-```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .venv\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
-
-```
-$ pip install -r requirements.txt
-```
-
-At this point you can now synthesize the CloudFormation template for this code.
-
-```
-$ cdk synth
-```
-
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
-
-## Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
+デプロイが成功したらAWSコンソールを開いて、ECSサービスで動作しているタスクの「ネットワーキング」タブを開いてください。
+タスクに割り当てられたIPv6アドレスをコピーして、HTTPでアクセスできた動作確認に成功です。
--- cdk_ipv6_subnets/cdk_ipv6_subnets_stack.py
+++ cdk_ipv6_subnets/cdk_ipv6_subnets_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     Stack,
+    ScopedAws,
+    CfnOutput,
     aws_ecs as ecs,
+    aws_s3 as s3,
 )
 from aws_cdk.aws_iam import (
     ManagedPolicy,
@@ -16,6 +19,8 @@ class CdkIpv6SubnetsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        scoped_aws = ScopedAws(self)
+
         vpc = Ipv6Vpc(self, "Ipv6Vpc")
 
         cluster = ecs.Cluster(
@@ -25,11 +30,16 @@ class CdkIpv6SubnetsStack(Stack):
             enable_fargate_capacity_providers=True,
         )
 
+        log_bucket = s3.Bucket(self, "AppClusterLogs")
+        CfnOutput(self, "LogBucketName", value=log_bucket.bucket_name)
+
         task_role = Role(
             self,
             "EcsTaskRole",
             assumed_by=ServicePrincipal("ecs-tasks.amazonaws.com"),
         )
+        log_bucket.grant_put(task_role)
+
         exec_role = Role(
             self,
             "EcsTaskExecRole",
@@ -50,12 +60,31 @@ class CdkIpv6SubnetsStack(Stack):
             # add_containerでloggingを渡すと、ECRにアクセスするActionがないロールを勝手に作るので、自分でつくって指定する
             execution_role=exec_role.without_policy_updates(),
         )
+        task_definition.add_firelens_log_router(
+            "fluent-bit",
+            # dockerhubからpullする
+            image=ecs.ContainerImage.from_registry("amazon/aws-for-fluent-bit"),
+            firelens_config=ecs.FirelensConfig(
+                type=ecs.FirelensLogRouterType.FLUENTBIT,
+            ),
+        )
         task_definition.add_container(
             "app",
             # NOTE: これはdockerhubからpullしてくる
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
             # NOTE: ECRはIPv6に対応していないのでECRからイメージをpullできずタスクが起動しない
             # image=ecs.ContainerImage.from_registry("public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest"),
+            logging=ecs.FireLensLogDriver(
+                options={
+                    "Name": "s3",
+                    "Match": "*",
+                    "region": scoped_aws.region,
+                    "bucket": log_bucket.bucket_name,
+                    "endpoint": f"https://s3.dualstack.{scoped_aws.region}.amazonaws.com",
+                    "total_file_size": "1M",
+                    "use_put_object": "On",
+                },
+            ),
             # NOTE: CloudWatch LogsがIPv6に対応してないようなので、ロギングを有効にするとタスクが起動しない
             # logging=ecs.GenericLogDriver(
             #    log_driver="awslogs",
